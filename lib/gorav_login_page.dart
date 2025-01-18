651274b184df7d890a93566a48aa6bcf3d4bddcc
goravjhabakh <goravjhabakh@gmail.com>
2025-01-18 21:07:28 +0530

    login working properly - gorav

--- lib/gorav_login_page.dart
+++ lib/gorav_login_page.dart
@@ -1,3 +1,6 @@
+import 'package:bya/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'widgets/custom_input.dart';
 
@@ -8,17 +11,108 @@ class GoravLoginPage extends StatefulWidget {
 
 class _GoravLoginPage extends State<GoravLoginPage> {
   bool otpSent = false;
+  String verificationID = '';
+  bool isLoading = false;
   final TextEditingController phoneController = TextEditingController();
   final TextEditingController otpController = TextEditingController();
 
-  void handleButtonPress() {
-    if (!otpSent) {
-      print("Phone number: ${phoneController.text}");
+  Future<void> sendOTP(String phoneNumber) async {
+    try {
+      await FirebaseAuth.instance.verifyPhoneNumber(
+          phoneNumber: '+91$phoneNumber',
+          timeout: Duration(seconds: 10),
+          verificationCompleted: (PhoneAuthCredential credential) async {
+            await FirebaseAuth.instance.signInWithCredential(credential);
+            print('Number verified');
+          },
+          verificationFailed: (FirebaseAuthException e) {
+            print('Verification Failed');
+          },
+          codeSent: (String verificationId, int? resendToken) {
+            setState(() {
+              otpSent = true;
+              verificationID = verificationId;
+              isLoading = false;
+            });
+          },
+          codeAutoRetrievalTimeout: (String verificationId) {
+            setState(() {
+              verificationID = verificationId;
+            });
+          });
+    } catch (e) {
       setState(() {
-        otpSent = true;
+        isLoading = false;
       });
+      print(e);
+    }
+  }
+
+  Future<bool> verifyOTP(String otp) async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: verificationID, smsCode: otp);
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  void handleButtonPress() async {
+    if (!otpSent) {
+      String phoneNumber = phoneController.text;
+      print("Phone number: ${phoneNumber}");
+      if (phoneNumber.isEmpty ||
+          phoneNumber.length != 10 ||
+          !RegExp(r'^\d{10}$').hasMatch(phoneNumber)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              "Enter a valid 10-digit phone number",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 12,
+              ),
+            ),
+            backgroundColor: Colors.transparent,
+            behavior: SnackBarBehavior.floating,
+            duration: Duration(seconds: 2),
+            elevation: 0,
+          ),
+        );
+        return;
+      }
+      await sendOTP(phoneNumber);
     } else {
-      print("OTP: ${otpController.text}");
+      String otp = otpController.text;
+      print("OTP: ${otp}");
+      if (otp.isEmpty || otp.length != 6 || !RegExp(r'^\d{6}$').hasMatch(otp)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              "Enter valid OTP",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 12,
+              ),
+            ),
+            backgroundColor: Colors.transparent,
+            behavior: SnackBarBehavior.floating,
+            duration: Duration(seconds: 2),
+            elevation: 0,
+          ),
+        );
+        return;
+      }
+      bool LoggedIn = await verifyOTP(otp);
+      if (LoggedIn) {
+        print('Logged In Successfully');
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+      } else {
+        print('Invalid OTP');
+      }
     }
   }
 
@@ -79,20 +173,23 @@ class _GoravLoginPage extends State<GoravLoginPage> {
                   Padding(
                     padding:
                         const EdgeInsets.only(left: 10, right: 10, top: 30),
-                    child: OutlinedButton(
-                      onPressed: handleButtonPress,
-                      style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.white, width: 2.0),
-                          foregroundColor: Colors.white),
-                      child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                        child: Text(
-                          'Get OTP',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-                    ),
+                    child: isLoading
+                        ? CircularProgressIndicator()
+                        : OutlinedButton(
+                            onPressed: handleButtonPress,
+                            style: OutlinedButton.styleFrom(
+                                side:
+                                    BorderSide(color: Colors.white, width: 2.0),
+                                foregroundColor: Colors.white),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 12, horizontal: 24),
+                              child: Text(
+                                otpSent ? 'Login' : 'Get OTP',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
                   ),
                 ],
               ),
